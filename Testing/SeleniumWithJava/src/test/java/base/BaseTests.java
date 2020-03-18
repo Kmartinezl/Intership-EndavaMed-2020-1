f424b70033a8cc25bd5bcd2090544badf669c5b4
Keila Martinez <Keila.Martinez@endava.com>
2020-03-18 17:26:16 -0500

    Primera aproximación: WebDriver - Finding Web Elements y element Interaction con POM

--- Testing/SeleniumWithJava/src/test/java/base/BaseTests.java
+++ Testing/SeleniumWithJava/src/test/java/base/BaseTests.java
@@ -5,12 +5,15 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
+import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
-    //Para saber dónde está el ejecutable
     public void setUp() {
 
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver/chromedriver.exe");
@@ -18,15 +21,25 @@ public class BaseTests {
 
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
-        //driver.manage().window().fullscreen();
 
+        homePage = new HomePage(driver);
+
+        //driver.manage().window().fullscreen();
         //PARA TAMAÑO PERSONALIZADO
         //driver.manage().window().setSize(new Dimension(400, 838));
 
-        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        //EJERCICIO 1
+        /*WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
+        shiftingContent.click();
+
+        WebElement example1 = driver.findElement(By.linkText("Example 1: Menu Element"));
+        example1.click();
+
+        List<WebElement> links = driver.findElements(By.tagName("li"));
+        System.out.println(links.size());*/
 
         System.out.println(driver.getTitle());
-        driver.quit();
+        //driver.quit();
     }
 
     public static void main(String args[]) {
